7e5befefa13e7555565988f6f43055fdcc35a432
MLAbram <23126920+MLAbram@users.noreply.github.com>
2023-06-04 19:40:28 -0500

    code updates

--- routes/cookie.js
+++ routes/cookie.js
@@ -1,34 +1,47 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 const jwt = require('jsonwebtoken')
-const { promisify } = require('util')
+// const { promisify } = require('util')
 
 require('dotenv').config()
 
-exports.isLoggedIn = async (req, res, next) => {
-    if (req.cookies.myCookie) {
-        try {
-            // verify token exists
-            const decoded = await promisify(jwt.verify)(req.cookies.myCookie,
-                process.env.SALT_TOKEN
-            )
-
-            req.user = ({
-                id: '123456',
-                email: 'jd@email.com',
-                first_name: 'Jon', 
-                last_name: 'Doe'
-            })
-        } catch (error) {
-            next()
-        }
-    } else {
+exports.validateCookie = async (req, res, next) => {
+    const token = req.cookies.myCookie
+    console.log('token: ' + token)
+    try {
+        const user = jwt.verify(token, process.env.SALT_TOKEN)
+        console.log('user: ' + user)
+        req.user = user
         next()
-    }    
+    } catch (err) {
+        console.log('Err: ' + err)
+        // res.clearCookie('myCookie')
+        // return res.redirect('/')
+    }
+    
+    // if (req.cookies.myCookie) {
+    //     try {
+    //         verify token exists
+    //         const decoded = await promisify(jwt.verify)(req.cookies.myCookie,
+    //             process.env.SALT_TOKEN
+    //         )
+            
+    //         req.user = ({
+    //             id: '123456',
+    //             email: 'jd@email.com',
+    //             first_name: 'Jon', 
+    //             last_name: 'Doe'
+    //         })
+    //     } catch (error) {
+    //         next()
+    //     }
+    // } else {
+    //     next()
+    // }    
 }
 
-exports.loggedOut = async (req, res) => {
-    res.cookie('myCookie', 'LoggedOut', {
+exports.disableCookie = async (req, res) => {
+    res.cookie('myCookie', 'disableCookie', {
         expires: new Date(Date.now() + 1 * 1000),
         httpOnly: true
     })
--- routes/v1_hbs.js
+++ routes/v1_hbs.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
-const cookieController = require('./cookie')
+const cookieJwtAuth = require('./cookie')
 const jwt = require('jsonwebtoken')
 
 app.set('view engine', 'hbs')
@@ -14,59 +14,99 @@ app.use(bodyParser.json())
 
 hbs.registerPartials(process.cwd() + '/views')
 
-app.get('/', cookieController.isLoggedIn, (req, res) => {
+app.get('/', (req, res) => {
     return res.status(200).render('index', {
         navbar_title: 'Node.js Cookie Demo'
     })
 })
 
-app.get('/test-1', cookieController.isLoggedIn, (req, res) => {
+app.get('/test-1', cookieJwtAuth.validateCookie, (req, res) => {
     return res.status(200).render('test-1', {
         navbar_title: 'Node.js Cookie Demo | Test #1'
     })
 })
 
-app.get('/test-2', (req, res) => {
+app.get('/test-2', cookieJwtAuth.validateCookie, (req, res) => {
     return res.status(200).render('test-2', {
         navbar_title: 'Node.js Cookie Demo | Test #2'
     })
 })
 
-app.get('/enable-cookie', (req, res) => {
-    // create token; https://jwt.io/
-    const token = jwt.sign ({
-        id: '123456',
-        email: 'jd@email.com',
-        first_name: 'Jon', 
-        last_name: 'Doe'                                
-    }, 
-        process.env.SALT_TOKEN, 
-    {
-        expiresIn: process.env.SALT_EXPIRE
-    })
-
-    // set token to cookie
-    const cookieToken = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 43200000
-        ),
-            httpOnly: true
-    }
-
-    res.cookie('myCookie', token, cookieToken)
-
-    return res.status(200).render('index', {
-        cookie_active: true,
-        navbar_title: 'Node.js Cookie Demo',
+app.get('/profile', cookieJwtAuth.validateCookie, (req, res) => {
+    return res.status(200).render('profile', {
+        navbar_title: 'Node.js Cookie Demo | Profile',
         user: {
             id: '123456',
             email: 'jd@email.com',
             first_name: 'Jon', 
-            last_name: 'Doe'                                
+            last_name: 'Doe'  
         }
     })
 })
 
-app.get('/logout', cookieController.loggedOut)
+app.get('/faq', cookieJwtAuth.validateCookie, (req, res) => {
+    return res.status(200).render('faq', {
+        navbar_title: 'Node.js Cookie Demo | FAQ'
+    })
+})
+
+app.get('/enable-cookie', (req, res) => {
+    const user = {
+        id: '123456',
+        email: 'jd@email.com',
+        first_name: 'Jon', 
+        last_name: 'Doe'  
+    }
+
+    const token = jwt.sign(user, process.env.SALT_TOKEN, { expiresIn: '15m'})
+
+    res.cookie('myCookie', token, {
+        httpOnly: true,
+        secure: true,
+        signed: true
+    })
+
+    return res.status(200).render('index', {
+        navbar_title: 'Node.js Cookie Demo',
+        user: user
+    })
+
+    // // create token; https://jwt.io/
+    // const token = jwt.sign ({
+    //     id: '123456',
+    //     email: 'jd@email.com',
+    //     first_name: 'Jon', 
+    //     last_name: 'Doe'                                
+    // }, 
+    //     process.env.SALT_TOKEN, 
+    // {
+    //     expiresIn: process.env.SALT_EXPIRE
+    // })
+
+    // // set token to cookie
+    // const cookieToken = {
+    //     expires: new Date(
+    //         Date.now() + process.env.COOKIE_EXPIRE * 43200000
+    //     ),
+    //         httpOnly: true
+    // }
+
+    // res.cookie('myCookie', token, cookieToken, {
+    //     httpOnly: true
+    // })
+
+    // return res.status(200).render('index', {
+    //     cookie_active: true,
+    //     navbar_title: 'Node.js Cookie Demo',
+    //     user: {
+    //         id: '123456',
+    //         email: 'jd@email.com',
+    //         first_name: 'Jon', 
+    //         last_name: 'Doe'                                
+    //     }
+    // })
+})
+
+app.get('/logout', cookieJwtAuth.disableCookie)
 
 module.exports = app
